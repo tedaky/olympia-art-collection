6cf645172fb41efaa7cebfdb1d7017bfc99c9011
Eric Tiedeken <eric.tiedeken@gmail.com>
2018-09-29 18:43:15 -0500

    Navigation and top banner

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index/index', { title: ''});
+  res.render('index/index', { title: 'Olympia Art Collection'});
 });
 
 module.exports = router;
