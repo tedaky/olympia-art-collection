2a9e3725515ed585395997ad8574633b79ffa76e
Eric Tiedeken <eric.tiedeken@gmail.com>
2018-10-19 09:08:24 -0500

    Reformated for about page and modifications.

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 var sassMiddleware = require('node-sass-middleware');
 
 var indexRouter = require('./routes/index');
+var aboutRouter = require('./routes/about');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(sassMiddleware({
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/about/', aboutRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
